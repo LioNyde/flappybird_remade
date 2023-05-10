72a4e0b901307422cf31cf9c9d8cbc871575f882
LioNyde <balanggabriel09@gmail.com>
2023-05-10 20:26:16 +0800

    New Changes - in player sprite - in title scene - random posY for obstacles

--- src/gamescene.js
+++ src/gamescene.js
@@ -26,7 +26,7 @@ class Gamescene extends Phaser.Scene
         }, this);
 
 //      O B S T A C L E
-        this.pipe1 = new Pipes(this, game.scale.width, game.scale.height - 20, 'pipes')
+        this.pipe1 = new Pipes(this, game.scale.width,  480, 'pipes')
         this.pipeF1 = new PipesF(this, this.pipe1.x, this.pipe1.y - 410, 'pipes')
 
         this.pipe2 = new Pipes(this, game.scale.width + 430, 350, 'pipes')
@@ -49,19 +49,20 @@ class Gamescene extends Phaser.Scene
         }
         this.physics.world.once('overlap', function()
         {
-            //this.scene.start(this.scene.key)
+            this.scene.start(this.scene.key)
             
         }, this)
     }
 
     update(time, delta)
     {
+        let randomY = Phaser.Math.Between(350, 480)
 //      P A R A L L A X  B A C K G R O U N D
         this.background.tilePositionX += 3
         this.floor.tilePositionX +=  6
         
         this.fps.text = 'fps: ' + Math.floor(delta)
-        this.player.angle = (this.player.body.velocity.y > 0) ? 25 : - 25
+        this.player.angle = (this.player.body.velocity.y > 0) ? 15 : - 15
 
 //      SET WALLS VELOCITY
         this.pipe1.setVelocityX(-10 * delta)
@@ -78,16 +79,28 @@ class Gamescene extends Phaser.Scene
         {
             this.pipe1.x = this.pipe3.x + 430
             this.pipeF1.x = this.pipe1.x
+            this.pipe1.y = randomY
+            this.pipeF1.y = randomY - 430
         }
         if(this.pipe2.x <= 0)
         {
             this.pipe2.x = this.pipe1.x + 430
             this.pipeF2.x = this.pipe2.x
+            this.pipe2.y = randomY
+            this.pipeF2.y = randomY - 430
         }
         if(this.pipe3.x <= 0)
         {
             this.pipe3.x = this.pipe2.x + 430
             this.pipeF3.x = this.pipe3.x
+            this.pipe3.y = randomY
+            this.pipeF3.y = randomY - 430
         }
     }
 }
+/*
+--NEW
+-changes in player sprite
+-changes in title scene
+-randomY pos for obstacles
+*/
--- src/menuscene.js
+++ src/menuscene.js
@@ -15,26 +15,17 @@ class Menuscene extends Phaser.Scene
         this.load.image('pipes', 'assets/pipes.png')
         this.load.image('player', 'assets/player.png')
         this.load.image('sensor', 'assets/sensor.png')
+        this.load.image('title', 'assets/titlecard.png')
     }
 
     create()
     {
-        this.background = this.add.tileSprite(0, 0, config.width, config.height, 'background').setOrigin(0, 0)
-
-        this.playtxt = this.add.text(game.scale.width - 100, game.scale.height - 100, 'play', {color: 'black', fontSize: 30})
-            .setOrigin(0.5, 0.5)
+        this.title = this.add.sprite(0, 0, 'title')
+            .setOrigin(0, 0)
             .setInteractive({useHandCursor: true})
             .on('pointerdown', function() { this.scene.start('Gamescene')}, this)
-        ;
-        this.floor = this.add.tileSprite(this.centerX, game.scale.height - 32, 860, 64, 'floor')
-        //this.scene.start('Gamescene')
     }
 
-    update()
-    {
-        this.background.tilePositionX += 3
-        this.floor.tilePositionX += 6
-    }
 }
 
 Phaser.Physics.Matter.Matter.Engine._bodiesApplyGravity = function (bodies, gravity)
